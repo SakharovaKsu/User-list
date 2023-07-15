426b3a38dd56ca96e6557e6f3f67d76ad70c31ab
SakharovaKsu <nagis38@gmail.com>
2023-07-15 12:59:45 +0500

    Убрала лишние переменные в тестах

--- src/TaskReducer.test.ts
+++ src/TaskReducer.test.ts
@@ -4,17 +4,14 @@ import {TaskAssocType} from './App';
 import {addTodoListAC} from './TodoListReducer';
 
 test ('removing a task from the list', () => {
-    const todoListId1 = v1()
-    const todoListId2 = v1()
-
     const defTasks: TaskAssocType = {
-        [todoListId1]: [
+        'todolistId1': [
             {id: v1(), title: 'HTML&CSS', isDone: true},
             {id: v1(), title: 'JS', isDone: true},
             {id: v1(), title: 'React', isDone: false},
             {id: v1(), title: 'TS', isDone: false}
         ],
-        [todoListId2]: [
+        'todolistId2': [
             {id: v1(), title: 'HTML&CSS', isDone: true},
             {id: v1(), title: 'JS', isDone: true},
             {id: v1(), title: 'React', isDone: false},
@@ -22,27 +19,25 @@ test ('removing a task from the list', () => {
         ]
     }
 
-    const taskID = defTasks[todoListId1][0].id
+    const taskID = defTasks['todolistId1'][0].id
 
-    const action = removeTaskAC(todoListId1, taskID)
+    const action = removeTaskAC('todolistId1', taskID)
     const endTask = TasksReducer(defTasks, action)
 
-    expect(endTask[todoListId1].length).toBe(3)
-    expect(endTask[todoListId1][0].title).toBe('JS')
+    expect(endTask['todolistId1'].length).toBe(3)
+    expect(endTask['todolistId1'][0].title).toBe('JS')
 })
 
 test ('there should be a new task', () => {
-    const todoListId1 = v1()
-    const todoListId2 = v1()
 
     const defTasks: TaskAssocType = {
-        [todoListId1]: [
+        'todolistId1': [
             {id: v1(), title: 'HTML&CSS', isDone: true},
             {id: v1(), title: 'JS', isDone: true},
             {id: v1(), title: 'React', isDone: false},
             {id: v1(), title: 'TS', isDone: false}
         ],
-        [todoListId2]: [
+        'todolistId2': [
             {id: v1(), title: 'HTML&CSS', isDone: true},
             {id: v1(), title: 'JS', isDone: true},
             {id: v1(), title: 'React', isDone: false},
@@ -52,28 +47,26 @@ test ('there should be a new task', () => {
 
     const taskTitle = 'Vue'
 
-    const action = addTaskAC(todoListId1, taskTitle)
+    const action = addTaskAC('todolistId1', taskTitle)
     const endTask = TasksReducer(defTasks, action)
 
-    expect(endTask[todoListId1].length).toBe(5)
-    expect(endTask[todoListId2].length).toBe(4)
-    expect(endTask[todoListId1][0].title).toBe('Vue')
-    expect(endTask[todoListId1][0].isDone).toBe(false)
-    expect(endTask[todoListId1][0].id).toBeDefined()
+    expect(endTask['todolistId1'].length).toBe(5)
+    expect(endTask['todolistId2'].length).toBe(4)
+    expect(endTask['todolistId1'][0].title).toBe('Vue')
+    expect(endTask['todolistId1'][0].isDone).toBe(false)
+    expect(endTask['todolistId1'][0].id).toBeDefined()
 })
 
 test ('should change the title in the task', () => {
-    const todoListId1 = v1()
-    const todoListId2 = v1()
 
     const defTasks: TaskAssocType = {
-        [todoListId1]: [
+        'todolistId1': [
             {id: v1(), title: 'HTML&CSS', isDone: true},
             {id: v1(), title: 'JS', isDone: true},
             {id: v1(), title: 'React', isDone: false},
             {id: v1(), title: 'TS', isDone: false}
         ],
-        [todoListId2]: [
+        'todolistId2': [
             {id: v1(), title: 'HTML&CSS', isDone: true},
             {id: v1(), title: 'JS', isDone: true},
             {id: v1(), title: 'React', isDone: false},
@@ -81,27 +74,24 @@ test ('should change the title in the task', () => {
         ]
     }
 
-    const taskID = defTasks[todoListId1][0].id
+    const taskID = defTasks['todolistId1'][0].id
 
-    const action = updateTaskAC(todoListId1, taskID, 'CSS')
+    const action = updateTaskAC('todolistId1', taskID, 'CSS')
     const endTask = TasksReducer(defTasks, action)
 
-    expect(endTask[todoListId1][0].title).toBe('CSS')
-    expect(endTask[todoListId1][0].title).not.toBe(defTasks[todoListId1][0].title)
+    expect(endTask['todolistId1'][0].title).toBe('CSS')
+    expect(endTask['todolistId1'][0].title).not.toBe(defTasks['todolistId1'][0].title)
 })
 
 test ('should change the status in tasks', () => {
-    const todoListId1 = v1()
-    const todoListId2 = v1()
-
     const defTasks: TaskAssocType = {
-        [todoListId1]: [
+        'todolistId1': [
             {id: v1(), title: 'HTML&CSS', isDone: true},
             {id: v1(), title: 'JS', isDone: true},
             {id: v1(), title: 'React', isDone: false},
             {id: v1(), title: 'TS', isDone: false}
         ],
-        [todoListId2]: [
+        'todolistId2': [
             {id: v1(), title: 'HTML&CSS', isDone: true},
             {id: v1(), title: 'JS', isDone: true},
             {id: v1(), title: 'React', isDone: false},
@@ -109,13 +99,13 @@ test ('should change the status in tasks', () => {
         ]
     }
 
-    const taskID = defTasks[todoListId1][0].id
+    const taskID = defTasks['todolistId1'][0].id
 
-    const action = changeStatusTaskAC(todoListId1, taskID, false)
+    const action = changeStatusTaskAC('todolistId1', taskID, false)
     const endTask = TasksReducer(defTasks, action)
 
-    expect(endTask[todoListId1][0].isDone).toBe(false)
-    expect(endTask[todoListId1][0].isDone).not.toBe(defTasks[todoListId1][0].isDone)
+    expect(endTask['todolistId1'][0].isDone).toBe(false)
+    expect(endTask['todolistId1'][0].isDone).not.toBe(defTasks['todolistId1'][0].isDone)
 })
 
 test('new array should be added when new todolist is added', () => {
