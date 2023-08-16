f89fe5d80f4cb741aa4ffa42311e3d258d6d629c
SakharovaKsu <nagis38@gmail.com>
2023-08-16 12:52:15 +0500

    Добавление Snapshot tests для сторибука

---
+++ integration/addItemForm.test.js
@@ -0,0 +1,37 @@
+// describe('addItemForm', () => {
+//     it('base example, visually looks correct', async () => {
+//         // APIs from jest-puppeteer
+//         await page.goto('http://localhost:6006/iframe.html?args=&id=todolists-additemform--add-item-form-story&viewMode=story',
+//             {waitUntil: "networkidle2"});
+//
+//         const image = await page.screenshot();
+//
+//         // API from jest-image-snapshot
+//         expect(image).toMatchImageSnapshot();
+//     });
+// });
+
+const puppeteer = require('puppeteer');
+
+describe('addItemForm', () => {
+    let browser;
+    let page;
+
+    beforeAll(async () => {
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+
+    it('base example, visually looks correct', async () => {
+        await page.goto('http://localhost:6006/iframe.html?args=&id=todolists-additemform--add-item-form-story&viewMode=story',
+            {waitUntil: "networkidle2"});
+
+        const image = await page.screenshot();
+
+        expect(image).toMatchImageSnapshot();
+    });
+});
