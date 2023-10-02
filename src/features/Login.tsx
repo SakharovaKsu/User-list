da79da248b523a89e9c9fc79f2950f35719a93cd
SakharovaKsu <nagis38@gmail.com>
2023-10-02 19:30:19 +0300

    Валидация формы

--- src/features/Login.tsx
+++ src/features/Login.tsx
@@ -7,8 +7,43 @@ import FormGroup from '@mui/material/FormGroup';
 import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import {useFormik} from 'formik';
+
+type FormikErrorType = {
+    email?: string
+    password?: string
+    rememberMe?: boolean
+}
 
 export const Login = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {}
+            const regx = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+            if (!values.email) {
+                errors.email = 'Required'
+            } else if (!regx.test(values.email)) {
+                errors.email = 'Invalid email address'
+            }
+
+            if (!values.password) {
+                errors.email = 'Required'
+            } else if (values.password.length < 4) {
+                errors.password = 'Must be more 3 symbols'
+            }
+            return errors
+        },
+        onSubmit: (values) => {
+            alert(JSON.stringify(values));
+        },
+    })
+
     return <Grid container justifyContent={'center'}>
         <Grid item justifyContent={'center'}>
             <FormControl>
@@ -22,16 +57,42 @@ export const Login = () => {
                     <p>Email: free@samuraijs.com</p>
                     <p>Password: free</p>
                 </FormLabel>
-                <FormGroup>
-                    <TextField label="Email" margin="normal"/>
-                    <TextField type="password" label="Password"
-                               margin="normal"
-                    />
-                    <FormControlLabel label={'Remember me'} control={<Checkbox/>}/>
-                    <Button type={'submit'} variant={'contained'} color={'success'}>
-                        Login
-                    </Button>
-                </FormGroup>
+
+                {/* handleSubmit вызывает функцию onSubmit, которая находится в useFormik (собираем набранные данные) и */}
+                {/* в values залетает объект initialValues */}
+                <form onSubmit={formik.handleSubmit}>
+                    <FormGroup>
+                        <TextField label='Email'
+                                   margin='normal'
+
+                                   // засчет name, формик понимает куда записать значения в initialValues, если name не укажет, то и значение не добавит
+                                   name={'email'}
+                                   onBlur={formik.handleBlur}
+                                   onChange={formik.handleChange}
+                                   value={formik.values.email}
+                        />
+                        {formik.touched.email && formik.errors.email && <div style={{color: 'red'}}>{formik.errors.email}</div>}
+
+                        <TextField type='password'
+                                   label='Password'
+                                   margin='normal'
+                                   name={'password'}
+                                   onBlur={formik.handleBlur}
+                                   onChange={formik.handleChange}
+                                   value={formik.values.password}
+                        />
+                        {/* touched для того что б показал ошибку, после того как отработало поле*/}
+                        {formik.touched.password && formik.errors.password && <div style={{color: 'red'}}>{formik.errors.password}</div>}
+
+                        <FormControlLabel label={'Remember me'}
+                                          control={<Checkbox/>}
+                                          name={'rememberMe'}
+                        />
+                        <Button type={'submit'} variant={'contained'} color={'success'}>
+                            Login
+                        </Button>
+                    </FormGroup>
+                </form>
             </FormControl>
         </Grid>
     </Grid>
